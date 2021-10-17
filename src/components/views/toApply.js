8b345fcceeb27fa9e83a3eb52b533638197c4655
harshitasrivasatva <harshitasriavastava376@gmail.com>
2021-10-17 14:25:25 +0530

    managing the contrast of background

--- src/components/views/toApply.js
+++ src/components/views/toApply.js
@@ -1,134 +1,95 @@
-import * as React from 'react';
+import React from "react";
+
+// react-bootstrap components
 import {
-    Table, 
-    TableBody, 
-    TableCell,
-    TableContainer,
-    TableHead , 
-    TableRow,
-    Paper ,
-    Avatar,
-    Grid,
-    Typography,
-    TablePagination,
-    TableFooter
-} from '@material-ui/core';
+  Card,
+  Table,
+  Container,
+  Button
+} from "react-bootstrap";
 
-import faker from 'faker';
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-    table: {
-        minWidth: 650,
-    },
-    tableContainer: {
-        borderRadius: 15,
-        margin: "10px auto",
-        maxWidth: 950
-    },
-    tableHeaderCell: {
-        fontWeight: 'bold',
-        backgroundColor: theme.palette.primary.dark,
-        color: theme.palette.getContrastText(theme.palette.primary.dark)
-    },
-    avatar: {
-        backgroundColor: theme.palette.primary.light,
-        color: theme.palette.getContrastText(theme.palette.primary.light),
-        marginRight: '10px'
-    },
-    name : {
-        fontWeight: 'bold',
-        color: theme.palette.secondary.dark,
-    }
-
-}));
-
-let USERS = [];
-let STATUSES = ["Already Applied", "Not Applied" ];
-
-for(let i=0;i<20;i++){
-    USERS[i] = {
-        name: faker.name.findName(),
-        email: faker.internet.email(),
-        phone: faker.phone.phoneNumber(),
-        jobTitle: faker.name.jobTitle(),
-        company: faker.company.companyName(),
-        status: STATUSES[Math.floor(Math.random() * STATUSES.length)]
-    }
+function TableList() {
+  return (
+    <>
+      <Container fluid>
+            <Card className="strpied-tabled-with-hover">
+              <Card.Header>
+                <Card.Title as="h4">LIST OF ALL THE JOBS TO APPLY</Card.Title>
+              </Card.Header>
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover table-striped">
+                  <thead>
+                    <tr>
+                      <th className="border-0">ID</th>
+                      <th className="border-0">Company</th>
+                      <th className="border-0">Salary</th>
+                      <th className="border-0">Status</th>
+                      <th className="border-0">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Microsoft</td>
+                      <td>Rs. 360,738</td>
+                      <td>Already Applied!</td>
+                      <td>
+                      <Button className="btn-sm btn-success">
+                      Apply
+                      </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Goldman Sachs</td>
+                      <td>Rs. 230,789</td>
+                      <td>Not applied!</td>
+                      <td>
+                      <Button className="btn-sm btn-success">
+                      Apply
+                      </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Intuit Inc.</td>
+                      <td>Rs. 560,142</td>
+                      <td>Already Applied!</td>
+                      <td>
+                      <Button className="btn-sm btn-success">
+                      Apply
+                      </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Philips</td>
+                      <td>Rs. 380,735</td>
+                      <td>Already Applied!</td>
+                      <td>
+                      <Button className="btn-sm btn-success">
+                      Apply
+                      </Button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>D.E. Shaw</td>
+                      <td>Rs. 630,542</td>
+                      <td>Not Applied!</td>
+                      <td>
+                      <Button className="btn-sm btn-success">
+                      Apply
+                      </Button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+      </Container>
+    </>
+  );
 }
 
-function MyTable() {
-    const classes = useStyles();
-
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const handleChangePage = (event, newPage) => {
-      setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-      setRowsPerPage(+event.target.value);
-      setPage(0);
-    };
-
-
-    return (
-    <TableContainer component={Paper} className={classes.tableContainer}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.tableHeaderCell}>UserInfo</TableCell>
-            <TableCell className={classes.tableHeaderCell}>JobInfo</TableCell>
-            <TableCell className={classes.tableHeaderCell}>Satus</TableCell>
-            <TableCell className={classes.tableHeaderCell}>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {USERS.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((user) => (
-            <TableRow
-              key={user.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                <TableCell>
-                    <Grid container>
-                        <Grid item lg={2}>
-                            <Avatar alt={user.name} src='.' className= {classes.avatar} />
-                        </Grid>
-                        <Grid item lg={10}>
-                            <Typography className={classes.name}>{user.name} </Typography>
-                            <Typography color="textSecondary" variant="body2">{user.email}</Typography>
-                            <Typography color="textSecondary" variant="body2">{user.phone}</Typography>
-                        </Grid> 
-                    </Grid>
-                </TableCell>
-                <TableCell>
-                    <Typography  color="primary" variant="subtitle2"> {user.jobTitle} </Typography>
-                    <Typography  color="textSecondary" variant="body2"> {user.company}  </Typography>
-                </TableCell>
-                <TableCell>{user.status}</TableCell>
-                <TableCell>
-                    <button className="apply-btn">Apply</button>
-                </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-            <TablePagination
-                rowsPerPageOptions={[5, 25, 100]}
-                component="div"
-                count={USERS.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                onPageChange={handleChangePage}
-                onRowsPerPageChange={handleChangeRowsPerPage}
-            />    
-        </TableFooter>
-      </Table>
-    </TableContainer>
-    )
-}
-
-export default MyTable
-
-
+export default TableList;
